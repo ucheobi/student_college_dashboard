29a6e71a6038419197320d4a93f113fd2c8888d5
ucheobi <ucheobi2009@yahoo.com>
2025-09-12 13:51:24 +0200

    feat:implement header component

---
+++ app/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+type HeaderProps = {
+    name: string;
+    buttonComponent?: React.ReactNode;
+    isSmallText?: boolean;
+}
+
+const Header = ({ name, buttonComponent, isSmallText = false}: HeaderProps) => {
+
+  return (
+    <div className="mb-5 flex w-full items-center justify-between">
+        <h1 className={`${isSmallText ? "text-lg" : "text-2xl"} font-semibold dark:text-white`}>
+            {name}
+        </h1>
+        {buttonComponent}
+    </div>
+  )
+}
+
+export default Header;
